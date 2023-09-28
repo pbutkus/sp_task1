b097f23456a20d1b9e557f698033371792a1358e
Petras Butkus <p.butkus360@gmail.com>
2023-09-28 16:16:16 +0300

    implement task logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        start(scanner);
+    }
+
+    public static void start(Scanner scanner) {
+        System.out.println("Input an integer:");
+
+        int userInput = scanner.nextInt();
+        boolean isPalindrome = isPalindrome(userInput);
+        System.out.println(isPalindrome);
+    }
+
+    public static boolean isPalindrome(int userInput) {
+        String userInputString = String.valueOf(userInput);
+
+        String firstHalf  = userInputString.substring(0, userInputString.length() / 2);
+        String secondHalf = userInputString.substring(userInputString.length() / 2);
+
+        if (firstHalf.length() + 1 == secondHalf.length()) {
+            secondHalf = secondHalf.substring(1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(secondHalf);
+        sb.reverse();
+        return firstHalf.contentEquals(sb);
     }
 }
